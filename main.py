f70eb15192e197aa093241783cfca9ff19d2630c
wilcoxeg <wilcoxeg@gmail.com>
2017-04-24 14:46:21 -0400

    getting liams integrations running. Need to install a bunch of packages

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from wit import Wit
 import json
+from tools.places import query_loc
 
 w = Wit('KL2CAZCLTOXH2DWULLZF7J5SCKLLN4IL')
 
 print("[Hello!]: Hello!")
 chatting = True
 while chatting:
-	usr_input = input("--> ")
+	usr_input = input(":::: ")
 
 	if (usr_input == "q"):
 		chatting = False
@@ -16,4 +17,5 @@ while chatting:
 		print(response)
 		for entity in response["outcomes"][0]['entities']:
 			if (entity == "local_search_query"):
-				print(response["outcomes"][0]['entities'][entity][0]["value"])
+				search_query = response["outcomes"][0]['entities'][entity][0]["value"]
+				search_results = query_loc(search_query, "GOOGLE_MAPS_KEY", 5)
