1475714bd51345c12098811010c1aa7e39841cf4
Liam Geron <liams.geron@gmail.com>
2017-04-24 16:03:02 -0400

    Clean output

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from wit import Wit
 import json
-#from tools.places import query_loc
+from tools.places import query_loc
 
 w = Wit('KL2CAZCLTOXH2DWULLZF7J5SCKLLN4IL')
 maps_key = open('keys/places.key.txt', 'r').read()
@@ -9,15 +9,17 @@ maps_key = open('keys/places.key.txt', 'r').read()
 print("[Hello!]: Hello!")
 chatting = True
 while chatting:
-	usr_input = input(":::: ")
+    usr_input = input(":::: ")
 
-	if (usr_input == "q"):
-		chatting = False
-	else:
-		print("Contacting wit.ai ...")
-		response = w.get_message(usr_input)
-		print(response)
-		for entity in response["outcomes"][0]['entities']:
-			if (entity == "local_search_query"):
-				search_query = response["outcomes"][0]['entities'][entity][0]["value"]
-				search_results = query_loc(search_query, maps_key, 5)
+    if (usr_input == "q"):
+        chatting = False
+    else:
+        print("Contacting wit.ai ...")
+        response = w.get_message(usr_input)
+        for entity in response["outcomes"][0]['entities']:
+            if (entity == "local_search_query"):
+                search_query = response["outcomes"][0]['entities'][entity][0]["value"]
+                search_results = query_loc(search_query, maps_key, 5)
+                print("I've found several {}s near you.".format(search_query))
+                for i in range(len(search_results)):
+                    print(search_results[i][0][0])
