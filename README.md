0a74059161d2563ea1bfc332874b8b5f73aa9d03
Liam Geron <liams.geron@gmail.com>
2017-06-12 10:47:10 -0400

    Documentation/cleanup

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Hello
+
+A Chatbot for Women's Healthcare Services
+
+For inquiries about code please contact liams.geron@gmail.com
+
+## APIs
+
+The Chatbot relies heavily on several Google APIs in order to serve its functionality. You will need to sign up for these APIs in order to run the Chatbot on your end. Please inspect the tools/ folder to see how these keys are used.
+
+A list of all APIs used:
+- Google Places (https://developers.google.com/places/)
+- Google Directions (https://developers.google.com/maps/documentation/directions/)
+
+## Code Style
+
+The code style is from wit.ai's pywit, the python library for wit.ai (https://github.com/wit-ai/pywit). You can inspect either main.py or the examples/ folder in the pywit github page to see best practices for working with wit.ai's API.
+
+Tutorials for wit.ai's GUI can be found on it's homepage (https://wit.ai/docs) where you will be able to see the basic workflow for training entities and intents. It is suggested that you do not skip this step, as it will tell you more about how the overall codebase works. 
+
+## Login
+
+I believe you need to use my old transcendent github account in order to access the trained wit.ai model we have going, so here is the login to that:
+
+usrname: lgeron@t19s.com
+pwd: chatbotfuntimeshello
+
+Please if you have any questions email liams.geron@gmail.com and I'll get back to you ASAP.
--- main.py
+++ main.py
@@ -1,58 +1,124 @@
+import sys, pickle, re
 from wit import Wit
-import json
-import re
+from datetime import datetime
+
+sys.path.insert(0, '~/Desktop/Programming/Work/Hello/tools/')
 from tools.places import query_loc, directions
 from tools.email_reminder import text_send, email_send
+from tools.find_sim_to_query import search_query
 
-w = Wit('KL2CAZCLTOXH2DWULLZF7J5SCKLLN4IL')
-maps_key = open('keys/places.key.txt', 'r').read()
-geo_key = open('keys/geocode.key.txt', 'r').read()
+if len(sys.argv) != 2:
+    print('usage: python ' + sys.argv[0] + ' <wit-token>')
+    exit(1)
+access_token = sys.argv[1]
 
-print("[Hello!]: Hello!")
-chatting = True
-while chatting:
-    usr_input = input(":::: ")
+texts = pickle.load(open('texts.p', 'rb'))
 
-    if (usr_input == "q"):
-        chatting = False
-    else:
-        print("Contacting wit.ai ...")
-        response = w.get_message(usr_input)
-        for entity in response["outcomes"][0]['entities']:
-            if entity == "local_search_query":
-                search_query = response["outcomes"][0]['entities'][entity][0]["value"]
-                search_results = query_loc(search_query, maps_key, 5)
-                print("There was more than one {} near you.".format(search_query))
-                for i in range(len(search_results)):
-                    print(search_results[i][0][0])
-                print("Would you like me to send directions to {}?".format(search_results[0][0][0]))
-                usr_input = input(":::: ")
-                response = w.get_message(usr_input)
-                for entity in response["outcomes"][0]['entities']:
-                    print(response["outcomes"][0]['entities'][entity][0]["value"])
-                    if response["outcomes"][0]['entities'][entity][0]["value"] == "affirmative":
-                        res = directions(geo_key, search_results[0][0][1])
-                        print("Ok, here are the directions:")
-                        for i in res[0]['legs'][0]['steps']:
-                            print(re.sub(r'<.*?>', '', i['html_instructions']))
-                    else:
-                        print("Ok no problem!")
-            elif entity == "reminder":
-                reminder = response["outcomes"][0]['entities'][entity][0]["value"]
-                print("When would you like me to remind you of that?")
-                usr_input = input(":::: ")
-                response = w.get_message(usr_input)
-                for entity in response["outcomes"][0]['entities']:
-                    if entity == "duration":
-                        t = response["outcomes"][0]['entities'][entity][0]['normalized']['value']
-                    else:
-                        print("I'm sorry, I didn't understand that time phrase. Please input in a more normal format ya dingus.")
-                        pass
-                print("And would you like me to do that via messaging or email?")
-                usr_input = input(":::: ")
-                if usr_input == "text":
-                    text_send(reminder, t)
-                else:
-                    print("And what email should I send that to?")
-                    email = input(":::: ")
-                    email_send(email, reminder, t)
+# Quickstart example
+# See https://wit.ai/ar7hur/Quickstart
+# Full run command: python alt_main.py 2CCTOZRLLGMPT3L6A3VEFRMAX6QKFBPT
+# TODO: Email reminders need to be hosted on a server
+# TODO: Handle out of story solutions
+
+def first_entity_value(entities, entity):
+    if entity not in entities:
+        return None
+    val = entities[entity][0]['value']
+    if not val:
+        return None
+    return val['value'] if isinstance(val, dict) else val
+
+def send(request, response):
+    print(response['text'])
+
+def update_location(request):
+    context = request['context']
+    entities = request['entities']
+
+    context['local_search_query'] = entities['local_search_query'][0]['value']
+    return context
+
+def send_location(request):
+    key = open('keys/places.key.txt', 'r').read()
+    context = request['context']
+
+    locations = query_loc(context['local_search_query'], key, n=1)
+    context['location'] = locations[0][0][1]
+    print("I've found the nearest {} to you:".format(context['local_search_query']))
+    for l in locations[0][0]:
+        print(l)
+    print("Distance is {}".format(locations[0][1]))
+    return context
+
+def process_yesno(request):
+    context = request['context']
+    entities = request['entities']
+    context['yesno'] = entities['yesno'][0]['value']
+
+    return context
+
+def send_directions(request):
+    key = open('keys/geocode.key.txt', 'r').read()
+    context = request['context']
+    try:
+        loc = context['location']
+    except KeyError:
+        print("I'm sorry, I'm not sure where you would like to go. Please search for a location first before asking for directions.")
+        return context
+    if not isinstance(context['location'], unicode):
+        loc = context['location'][-1]
+    directions_result = directions(key, loc)
+    for step in directions_result[0]['legs'][0]['steps']:
+        if 'steps' in step:
+            print(re.sub('<.*?>', '', step['steps'][0]['html_instructions']))
+        else:
+            print(re.sub('<.*?>', '', step['html_instructions']))
+    if 'arrival_time' in directions_result[0]['legs'][0]:
+        arrival_time = directions_result[0]['legs'][0]['arrival_time']
+        print("Estimated arrival time: {0} {1}".format(arrival_time['text'], arrival_time['time_zone']))
+    print("Let me know if I can help you with anything else.")
+    del context['location']
+    return context
+
+def store_remind_message(request):
+    context = request['context']
+    entities = request['entities']
+    context['remind_message'] = entities['remind_message'][0]['value']
+    context['time_to_reminder'] = entities['datetime'][0]['value']
+
+    return context
+
+def send_remind_message(request):
+    context = request['context']
+    time = context['time_to_reminder']
+    remind_message = context['remind_message']
+
+    text_send(remind_message, time)
+    del context['time']
+    del context['remind_message']
+    return context
+
+def process_query(request):
+    context = request['context']
+    entities = request['entities']
+
+    query = [entities['query'][0]['value']]
+    text = search_query(query, texts, 5)
+    for t in text:
+        print(t)
+    return context
+
+
+actions = {
+    'send': send,
+    'update_location': update_location,
+    'send_location': send_location,
+    'process_yesno': process_yesno,
+    'send_directions': send_directions,
+    'store_remind_message': store_remind_message,
+    'send_remind_message': send_remind_message,
+    'process_query': process_query
+}
+
+client = Wit(access_token=access_token, actions=actions)
+client.interactive()
